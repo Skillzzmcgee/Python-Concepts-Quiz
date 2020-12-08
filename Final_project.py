c121b04e167b0a8a5ee924f4f40c867bcd057ae0
Skillzzmcgee <73539981+Skillzzmcgee@users.noreply.github.com>
2020-12-07 22:54:15 -0600

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -6,9 +6,9 @@ Description: This program consists of a breezypythongui based interface, which i
 the directions/instructions of the game, followed by the quiz beginning once the instructions box is closed.
 The quiz is multiple-choice based and consists of 10 questions based on Python concepts, which uses a text file
 to read the questions from. The user will select the radio-button answer in which they believe is the correct
-answer to the question displayed, followed by selecting the "submit" button. If the users answer to the question
-was correct it will be displayed as either "Correct", or "Incorrect" within the results box. Once all 10 questions
-have been answered, the user will be prompted of their score out of 10(for example 6/10, 4/10, 9/10, etc).
+answer to the question displayed, followed by selecting the "submit" button. If the users answer to the question 
+was correct, it will be displayed as "Correct", or "Incorrect" if the answer was incorrect, within the results box. 
+Once all 10 questions have been answered, the user will be prompted of their score out of 10(for example 6/10, 4/10, 9/10, etc).
 """
 
 from breezypythongui import *
